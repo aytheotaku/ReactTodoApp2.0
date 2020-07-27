abf22f35fec477faa5e14c1521dcacaaa85a6175
Ayomide Akinbo <ayakinbo@gmail.com>
2020-07-27 15:51:49 +0100

    Updates

---
+++ precache-manifest.564e231a5bc802228a3a8b3e8697159e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059ca6b969e461551ae1cb93a68413b2",
+    "url": "/ReactTodoApp2.0/index.html"
+  },
+  {
+    "revision": "ac67397587b4c8a7281d",
+    "url": "/ReactTodoApp2.0/static/css/main.8837b2e2.chunk.css"
+  },
+  {
+    "revision": "a67a1b34c80d3607f440",
+    "url": "/ReactTodoApp2.0/static/js/2.8065f7a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTodoApp2.0/static/js/2.8065f7a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac67397587b4c8a7281d",
+    "url": "/ReactTodoApp2.0/static/js/main.cdb7bed6.chunk.js"
+  },
+  {
+    "revision": "98eb0ec657e39bd4cac7",
+    "url": "/ReactTodoApp2.0/static/js/runtime-main.26faf243.js"
+  }
+]);
